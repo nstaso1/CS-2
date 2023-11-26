88930e765501a66494c6afdbc17850cdc8a02946
nstaso1 <117488392+nstaso1@users.noreply.github.com>
2023-11-25 22:35:07 -0800

    Create Hospital.h

---
+++ Assignment18/Hospital.h
@@ -0,0 +1,20 @@
+#ifndef HOSPITAL_H
+#define HOSPITAL_H
+
+#include "Doctor.h"
+#include "Nurse.h"
+
+
+class Hospital
+{
+
+public:
+    Hospital();
+private:
+
+    Doctor d1;
+    Nurse n1;
+    
+};
+
+#endif
