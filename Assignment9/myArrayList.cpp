f4e4c86f35d80e84dd519d4441792b064eb48a05
nstaso1 <117488392+nstaso1@users.noreply.github.com>
2023-12-12 18:47:50 -0800

    Update myArrayList.cpp

--- Assignment9/myArrayList.cpp
+++ Assignment9/myArrayList.cpp
@@ -1,106 +1,68 @@
+#include <iostream>
 
 #include "myArrayList.h"
-#include <iostream>
+
 using namespace std;
 
-
-
-void myArrayList::insertE(int e)
+void ArrayList::insert(int val)
 {
-
-    
-    if (length < SIZE)
-    {
-         array[length] = e;
-         length++;
-
-        cout << "New element added. New array is: " << endl;
-        for (int i = 0; i < length; i++)
-        {
-            cout << array[i] << " ";
-        }
-        cout << endl;
-    }
-
-    else if (length == SIZE)
-    {
+    if (isF()) {
         cout << "Array is full." << endl;
+        return;
     }
-    
 
-    
-
+    numss[elementss] = val;
+    elementss++;
 }
-void myArrayList::removeE()
-{
-    
-    
-    if (length > 0)
-    {
-        
-        length--;
 
-        cout << "Last element array removed. New array is: " << endl;
-        for (int i = 0; i < length; i++)
-        {
-            cout << array[i] << " ";
+void ArrayList::removeE()
+{
+    numss[elementss - 1] = 0;
+    elementss--;
+}
+
+bool ArrayList::isF()
+{
+    return sizeof(numss) / sizeof(numss[0]) == elementss;
+}
+
+int ArrayList::find(int index)
+{
+    return numss[index];
+}
+
+int ArrayList::getL()
+{
+    return elementss;
+}
+
+void ArrayList::makeE()
+{
+    for (int i = 0 ; i < elementss ; i++)
+    {
+        numss[i] = 0;
+    }
+
+    elementss = 0;
+}
+
+void ArrayList::showV()
+{
+    if (elementss == 0) {
+        cout << "Array empty" << endl;
+        return;
+    }
+
+    cout << "Array values: ";
+
+    for (int i = 0 ; i < elementss ; i++)
+    {
+        cout << numss[i];
+
+        if (i == elementss - 1) {
+            cout << endl;
+        } else {
+            cout << ", ";
         }
-        cout << endl;
-    }
-
-    else if (length == 0)
-    {
-        cout << "Array is empty." << endl;
-    }
-
-
-}
-void myArrayList::isFull()
-{
-    if (length == SIZE)
-    {
-        cout << "The array is full." << endl;
-    }
-    else if (length < SIZE)
-    {
-        cout << "The array is not full. There is " << SIZE - length << " elements left to put in." << endl;
     }
 }
-void myArrayList::findE(int value)
-{
-    
-
-    for (int i = 0; i < length; i++)
-    {
-        if (array[i] == value)
-        {
-            cout << value << " is in the array." << endl;
-            return;
-        }
-    }
-    
-    cout << value << " is not in the array." << endl;
-
-}
-void myArrayList::getLength()
-{
-    cout << "The array length is: " << length << endl;
-}
-
-void myArrayList::makeEmpty()
-{
-    length = 0;
-    cout << "The array is now empty." << endl;
-}
-void myArrayList::showValues()
-{
-   cout << "Here are current values of the array:" << endl;
-    for (int i = 0; i < length; i++)
-    {
-        cout << array[i] << " ";
-    }
-    cout << endl;
-}
-
-
-
